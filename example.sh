d8830c59c709cab3f5ac3442d5aea176f711a0a0
fabricio policarpo <fabriciopolicarpo0@gmail.com>
2022-01-24 10:47:37 -0500

    adding http

--- example.sh
+++ example.sh
@@ -1,7 +1,9 @@
-curl  'localhost:5001' \
--H 'Content-Type: application/json' \
--H 'Accept: application/json' \
--d '{error: "the server has died", reason: "I really dont know"}' -v --http1.1 POST
+curl  'localhost:9700' \
+  -H 'Content-Type: application/json' \
+  -H 'Accept: application/json' \
+  -X POST \
+  -d '{"name":"mister seitan"}' -v --http1.1
+# -d '{"message": "this is a message from container running in aws"}'  
 
-echo "your message goes here" | nc -c localhost 5001
-echo el.logs | nc -c localhost 5001
+# echo "your message goes here" | nc -c localhost 5001
+# echo el.logs | nc -c localhost 5001
